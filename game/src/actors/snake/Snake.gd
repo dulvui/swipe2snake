d451a79d78a0c0097ac42950f2b34bc2971c48ff
Simon Dalvai <git@s9i.cc>
2023-07-24 18:25:18 +0200

    snake: craete base block

---
+++ game/src/actors/snake/Snake.gd
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2023 Simon Dalvai <info@simondalvai.com>
+
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+extends Node2D
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
