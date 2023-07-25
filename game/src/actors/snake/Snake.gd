81baf3ccd25b28240afb59b8d6019591d5419d8e
Simon Dalvai <git@s9i.cc>
2023-07-25 23:56:03 +0200

    snake: basic movement with buffer and valid move check

--- game/src/actors/snake/Snake.gd
+++ game/src/actors/snake/Snake.gd
@@ -2,11 +2,63 @@
 
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-extends Sprite2D
+extends Node2D
 
-func _ready() -> void:
-	pass
+enum DIRECTION {UP, DOWN, NONE, LEFT, RIGHT}
 
+const SIZE:int = 64
 
-func _process(delta: float) -> void:
-	pass
+var direction:int = DIRECTION.NONE
+var direction_buffer:Array = []
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("move_up"):
+		_add_to_buffer(DIRECTION.UP)
+	elif event.is_action_pressed("move_down"):
+			_add_to_buffer(DIRECTION.DOWN)
+	elif event.is_action_pressed("move_left"):
+		_add_to_buffer(DIRECTION.LEFT)
+	elif event.is_action_pressed("move_right"):
+		_add_to_buffer(DIRECTION.RIGHT)
+
+func _on_timer_timeout() -> void:
+	_move()
+
+func _move() -> void:
+	# check buffer for new direction
+	if not direction_buffer.is_empty():
+		direction = direction_buffer.pop_front()
+	
+	# create and configure tween
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_CUBIC)
+	
+	# actually move
+	match direction:
+		DIRECTION.UP:
+			tween.tween_property(self, "position", position + Vector2(0,-SIZE), 1)
+		DIRECTION.DOWN:
+			tween.tween_property(self, "position", position + Vector2(0,SIZE), 1)
+		DIRECTION.LEFT:
+			tween.tween_property(self, "position", position + Vector2(-SIZE,0), 1)
+		DIRECTION.RIGHT:
+			tween.tween_property(self, "position", position + Vector2(SIZE,0), 1)
+		_:
+			pass
+	
+
+func _add_to_buffer(new_direction:int) -> void:
+	if _is_valid_direction(new_direction):
+		direction_buffer.append(new_direction)
+	
+
+func _is_valid_direction(new_direction:int) -> bool:
+	# up = 0, down = 1, none = 2, left = 3, right = 4
+	# up - down = -1
+	# down - up = 1
+	# down - down = 0
+	# => valid move is > 1
+	if direction_buffer.size() == 0:
+		return abs(new_direction - direction) > 1
+	return abs(new_direction - direction_buffer[-1]) > 1
--- game/src/actors/snake/block/Block.gd
+++ game/src/actors/snake/block/Block.gd
@@ -2,11 +2,13 @@
 
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
-extends Sprite2D
+extends Node2D
 
-func _ready() -> void:
+enum DIRECTION {UP, DOWN, LEFT, RIGHT}
+
+func move(direction:int) -> void:
 	pass
 
 
-func _process(delta: float) -> void:
-	pass
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
