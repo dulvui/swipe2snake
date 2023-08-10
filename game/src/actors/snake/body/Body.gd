49a58b0e9b7cf72b1d75b79e39eb7c550bab87c7
Simon Dalvai <git@s9i.cc>
2023-08-10 14:39:05 +0200

    snake collides now with blocks and body; game over is woorking

--- game/src/actors/snake/body/Body.gd
+++ game/src/actors/snake/body/Body.gd
@@ -8,14 +8,18 @@ signal colission
 
 const SIZE:int = 64
 
+@export var is_head:bool = false
+
+@onready var area_2d:Area2D = $Area2D
 var direction:int
 
 var wait_one_move:bool = true
 
 func move(new_direction:int, time:float) -> void:
-	if wait_one_move:
+	if wait_one_move and not is_head:
 		wait_one_move = false
 		return
+	area_2d.monitorable = true
 	
 	# finish tween slighty before next timeout
 	# so blocks remain aligned 
@@ -42,5 +46,4 @@ func move(new_direction:int, time:float) -> void:
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	if not wait_one_move:
-		emit_signal("colission")
+	emit_signal("colission")
